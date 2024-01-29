63f8b634fda8c2700cc5ece7d2adfd1268a20dcd
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-29 15:38:46 -0300

    plotly

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.express as px
 
 
 df_encuesta = pd.read_excel('datos/Resultados conuslta ciudadana.xlsx', sheet_name='Base de datos')
