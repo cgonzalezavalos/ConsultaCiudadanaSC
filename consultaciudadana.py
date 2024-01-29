5dfec94e3c18299b53590dc1b9db2227182a497a
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-29 16:15:02 -0300

    correccion

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -11,8 +11,8 @@ nombres_columnas = df_encuesta.columns
 df_encuesta.rename(columns={nombres_columnas[0]: 'Portal',nombres_columnas[1]: 'Ultima_Postulacion',nombres_columnas[2]:'Nota_facilidad_postulación',nombres_columnas[3]:'Nota_pertinencia_info_solicitada',nombres_columnas[4]:'Contactada_en_proceso',nombres_columnas[5]:'Nota_calidad_evaluacion_realizada',nombres_columnas[6]:'Nota_oportunidad_entrega_resultados',nombres_columnas[7]:'Nota_proceso_reclutamiento_seleccion',nombres_columnas[8]:'comentario_sugerencia_proceso_postulación',nombres_columnas[9]:'informacion_relevante_sobre_instituciones_publicas'}, inplace=True)
 
 
-tb_portal=df_encuesta.groupby(['Portal','genero','rango_etario','region','discapacidad']).agg(Respuestas={'genero':'count'})
-tb_portal=tb_portal.rename()
+tb_portal = df_encuesta.groupby(['Portal', 'genero', 'rango_etario', 'region', 'discapacidad']).agg(Respuestas=('genero', 'count')).reset_index()
+
 
 
 with st.container():
