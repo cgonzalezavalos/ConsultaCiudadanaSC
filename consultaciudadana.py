6e32434dcae2f221afb8d1f1e456eb3a3f590a65
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-29 11:44:17 -0300

    Update consultaciudadana.py

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -9,4 +9,4 @@ df_encuesta['count_discapacidad']=np.where(df_encuesta['discapacidad']=='Sí',1,
 nombres_columnas = df_encuesta.columns
 df_encuesta.rename(columns={nombres_columnas[0]: 'Portal',nombres_columnas[1]: 'Ultima_Postulacion',nombres_columnas[2]:'Nota_facilidad_postulación',nombres_columnas[3]:'Nota_pertinencia_info_solicitada',nombres_columnas[4]:'Contactada_en_proceso',nombres_columnas[5]:'Nota_calidad_evaluacion_realizada',nombres_columnas[6]:'Nota_oportunidad_entrega_resultados',nombres_columnas[7]:'Nota_proceso_reclutamiento_seleccion',nombres_columnas[8]:'comentario_sugerencia_proceso_postulación',nombres_columnas[9]:'informacion_relevante_sobre_instituciones_publicas'}, inplace=True)
 
-st.dataframe(df_encuesta.head(5)
+st.dataframe(df_encuesta.head(5))
