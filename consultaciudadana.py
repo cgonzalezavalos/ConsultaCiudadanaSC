3a5bc23d6557df06cdbebf73d253c4b431479870
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-29 12:11:03 -0300

    Update consultaciudadana.py

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 df_encuesta = pd.read_excel('datos/Resultados conuslta ciudadana.xlsx', sheet_name='Base de datos')
