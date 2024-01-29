8f0f835c51827446f8720527e2a5cae0e7d4c7ea
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-29 11:54:40 -0300

    primera publicacion

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df_encuesta = pd.read_excel('datos/Resultados conuslta ciudadana.xlsx', sheet_name='Base de datos')
 df_encuesta['count_discapacidad']=np.where(df_encuesta['discapacidad']=='Sí',1,0)
