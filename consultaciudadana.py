6e143e70d0a39c233ffcde941663d47b19497200
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-30 12:52:04 -0300

    afirmacion_order

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -213,10 +213,11 @@ with st.container():
 
 # Define el orden deseado para la categoría 'genero'
 ult_post_order = ['Menos de un mes','Entre un mes y seis (6) meses', 'Más de seis (6) meses y menos de un año', 'Más de un año y menos de tres años','Hace más de tres años']     
+afirmacion_order = ['Si','No', 'No aplica']     
 graf_g1=px.bar(tb_g1, x='genero', y='Respuestas',color='Ultima_Postulacion',barmode='group' ,title='Hace cuanto fue la última postulación?',category_orders={'Ultima_Postulacion': ult_post_order})
 graf_g2=px.bar(tb_g2, x='genero', y='Respuestas',color='Nota_facilidad_postulación',barmode='group' ,title='Cuan fácil fue la última postulación?',category_orders={'Ultima_Postulacion': ult_post_order})
 graf_g3=px.bar(tb_g3, x='genero', y='Respuestas',color='Nota_pertinencia_info_solicitada',barmode='group' ,title='Cuan pertinente es la información solicitada en la postulación?',category_orders={'Ultima_Postulacion': ult_post_order})
-graf_g4=px.bar(tb_g4, x='genero', y='Respuestas',color='Contactada_en_proceso',barmode='group' ,title='Fue contactada para entregar feedback del proceso?')
+graf_g4=px.bar(tb_g4, x='genero', y='Respuestas',color='Contactada_en_proceso',barmode='group' ,title='Fue contactada para entregar feedback del proceso?',category_orders={'afirmacion_order'})
 
 with st.container():
     st.plotly_chart(graf_g1, use_container_width=True)
