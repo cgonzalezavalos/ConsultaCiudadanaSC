c74a20c1ccb6553ed555e4bd59d069c8253add0c
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-30 12:59:00 -0300

    df_consulta.query('Portal'!='Nunca he postulado')

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -45,6 +45,7 @@ def encuesta():
     df_consulta.rename(columns={nombres_columnas[0]: 'Portal',nombres_columnas[1]: 'Ultima_Postulacion',nombres_columnas[2]:'Nota_facilidad_postulación',nombres_columnas[3]:'Nota_pertinencia_info_solicitada',nombres_columnas[4]:'Contactada_en_proceso',nombres_columnas[5]:'Nota_calidad_evaluacion_realizada',nombres_columnas[6]:'Nota_oportunidad_entrega_resultados',nombres_columnas[7]:'Nota_proceso_reclutamiento_seleccion',nombres_columnas[8]:'comentario_sugerencia_proceso_postulación',nombres_columnas[9]:'informacion_relevante_sobre_instituciones_publicas'}, inplace=True)
     df_consulta.fillna('No aplica', inplace=True)
     df_consulta['contador']=1
+    df_consulta=df_consulta.query('Portal'!='Nunca he postulado')
     return df_consulta
 
 df_encuesta = encuesta()
