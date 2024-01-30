f55d722832baee452ae3eddf1198895deb58a3c3
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-30 08:27:55 -0300

    titulo y config

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -4,6 +4,31 @@ import streamlit as st
 import plotly.express as px
 
 
+st.set_page_config(layout='wide')
+
+# Set Page Header
+st.header("Resultados Encuesta Ciuadadana Servicio Civil")
+# Set custom CSS for hr element
+st.markdown(
+    """
+        <style>
+            hr {
+                margin-top: 0.0rem;
+                margin-bottom: 0.5rem;
+                height: 3px;
+                background-color: #333;
+                border: none;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Add horizontal line
+st.markdown("<hr>", unsafe_allow_html=True)
+
+
+
 @st.cache_data
 def encuesta():
     df_consulta=pd.read_excel('datos/Resultados conuslta ciudadana.xlsx', sheet_name='Base de datos')
