b355cbc534c0b2a6d0413808cb0ca5d2baf0d9e6
cgonzalezavalos <108363597+cgonzalezavalos@users.noreply.github.com>
2024-01-30 13:59:07 -0300

    formato rename df_consulta

--- consultaciudadana.py
+++ consultaciudadana.py
@@ -42,10 +42,10 @@ def encuesta():
     df_consulta=pd.read_excel('datos/Resultados conuslta ciudadana.xlsx', sheet_name='Base de datos')
     df_consulta['count_discapacidad']=np.where(df_consulta['discapacidad']=='Sí',1,0)
     nombres_columnas = df_consulta.columns
-    df_consulta.rename(columns={nombres_columnas[0]: 'Portal',nombres_columnas[1]: 'Ultima_Postulacion',nombres_columnas[2]:'Nota_facilidad_postulación',.\
-                                nombres_columnas[3]:'Nota_pertinencia_info_solicitada',nombres_columnas[4]:'Contactada_en_proceso',.\
-                                    nombres_columnas[5]:'Nota_calidad_evaluacion_realizada',nombres_columnas[6]:'Nota_oportunidad_entrega_resultados',.\
-                                        nombres_columnas[7]:'Nota_proceso_reclutamiento_seleccion',nombres_columnas[8]:'comentario_sugerencia_proceso_postulación',.\
+    df_consulta.rename(columns={nombres_columnas[0]: 'Portal',nombres_columnas[1]: 'Ultima_Postulacion',nombres_columnas[2]:'Nota_facilidad_postulación',
+                                nombres_columnas[3]:'Nota_pertinencia_info_solicitada',nombres_columnas[4]:'Contactada_en_proceso',
+                                    nombres_columnas[5]:'Nota_calidad_evaluacion_realizada',nombres_columnas[6]:'Nota_oportunidad_entrega_resultados',
+                                        nombres_columnas[7]:'Nota_proceso_reclutamiento_seleccion',nombres_columnas[8]:'comentario_sugerencia_proceso_postulación',
                                             nombres_columnas[9]:'informacion_relevante_sobre_instituciones_publicas'}, inplace=True)
     df_consulta.fillna('No aplica', inplace=True)
     df_consulta['contador']=1
